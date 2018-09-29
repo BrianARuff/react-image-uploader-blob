f03978d61ac28183b288ff70ca58d3b8dafb85b1
BrianARuff <thelonewolf198927@gmail.com>
2018-09-29 01:35:00 -0400

    some style changes

--- src/App.js
+++ src/App.js
@@ -1,42 +1,65 @@
 import React, { Component } from "react";
-import Img from './Img';
+import Img from "./Img";
 import "./App.css";
 
 class App extends Component {
   state = {
-    image: null,
+    file: null
   };
 
-  uploadImage = (e) => {
+  uploadfile = e => {
     console.log(e.target.files[0]);
-    this.setState({
-      image: URL.createObjectURL(e.target.files[0])
-    })
+    if(e.target.files[0].type.match(/[jpg][jpeg][png]/gi)) {
+      this.setState({
+        file: URL.createObjectURL(e.target.files[0])
+      });
+    }
   };
 
   render() {
     return (
       <div className="container">
         <div className="row">
-          <div className="col-md-12">
-            <div className="m-5 d-flex flex-column justify-content-center align-items-center" style={{height: '100vh'}}>
-              <div style={{width: '300px'}}>
-              {
-                !this.state.image ? <input
-                className=""
-                type="file"
-                name="image"
-                onChange={this.uploadImage}
-              /> :
-              <h4 className="font-weight-bold display-4 text-center">Uploaded!</h4>
-              }
-              {
-                this.state.image ? <Img image={this.state.image} /> : null
-              }
-              {
-                this.state.image ? <button className="btn btn-success btn-block font-weight-bold m-3">Submit</button> : null
-              }
-              </div>
+          <div className="col-md-12 d-flex justify-content-center align-items-center">
+            <div
+              className="d-flex flex-column justify-content-center align-items-center"
+              style={{ height: "100vh" }}
+            >
+              <React.Fragment>
+                {!this.state.file ? (
+                  <React.Fragment>
+                    <input
+                      className=""
+                      type="file" name="file" id="file"
+                      onChange={this.uploadfile}
+                      style={{
+                        width: "0px",
+                        height: "0px",
+                        opacity: "0",
+                        overflow: "hidden",
+                        position: "absolute",
+                        zIndex: "-1"
+                      }}
+                    />
+                    <label htmlFor="file" style={{cursor: 'pointer'}}>
+                      <div className="d-flex justify-content-center">
+                      <img src="http://www.stickpng.com/assets/images/586ac158b6fc1117b60b275f.png" style={{height: '30px'}} alt="upload-icon" />
+                      </div>
+                      <p className="font-weight-bold m-3">Choose a File</p>
+                    </label>
+                  </React.Fragment>
+                ) : (
+                  <h4 className="font-weight-bold display-4 text-center">
+                    Image Uploaded!
+                  </h4>
+                )}
+                {this.state.file ? <Img file={this.state.file} /> : null}
+                {this.state.file ? (
+                  <button className="btn btn-success btn-block font-weight-bold mt-3">
+                    Submit
+                  </button>
+                ) : null}
+              </React.Fragment>
             </div>
           </div>
         </div>
--- src/Img.js
+++ src/Img.js
@@ -2,6 +2,6 @@ import React from 'react';
 
 export default function Img(props) {
   return (
-    <img style={{height: '100px'}} className="border rounded-circle mx-auto d-block img-fluid mt-3" src={props.image} alt="Preview"/>
+    <img style={{height: '300px'}} className="border rounded-circle mx-auto d-block img-fluid mt-3" src={props.file} alt="Preview"/>
   )
 }
